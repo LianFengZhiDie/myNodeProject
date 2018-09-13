bde6bc28bd0b6616f7710cdeb5f28064aa0cbc02
feng.mingyi <feng.mingyi@centchain.com>
2018-09-13 08:15:33 +0800

    将route参数传给start函数

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require("http");
 var url = require("url");
 
-function start() {
+function start(route) {
   function onRequest(request, response) {
     var pathname = url.parse(request.url).pathname;
     console.log("Request for " + pathname + " received!");
