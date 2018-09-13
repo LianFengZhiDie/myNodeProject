247022cfbff3f44638fa9ed8995601d94f4e5fa0
constantE <lianfengzhidie@sina.com>
2018-09-13 08:10:35 +0800

    将路由函数做为参数扩展start函数

--- server.js
+++ server.js
@@ -4,9 +4,10 @@ var url = require("url");
 function start() {
   function onRequest(request, response) {
     var pathname = url.parse(request.url).pathname;
-    console.log("Request for " + pathname + " received.");
+    console.log("Request for " + pathname + " received!");
+    route(pathname);
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello World");
+    response.write("Hello World!");
     response.end();
   }
 
