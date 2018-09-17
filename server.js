d2a67d6c41ec2335d33550c453237098dc448c2d
constantE <lianfengzhidie@sina.com>
2018-09-17 08:17:13 +0800

    重构server使其能返回相应内容响应给浏览器

--- server.js
+++ server.js
@@ -4,15 +4,16 @@ var url = require("url");
 function start(route, handle) {
   function onRequest(request, response) {
     var pathname = url.parse(request.url).pathname;
-    console.log("Request for " + pathname + " received!");
-    route(handle, pathname);
+    console.log("Request for " + pathname + " received.");
+
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello World!");
+    var content = route(handle, pathname)
+    response.write(content);
     response.end();
   }
 
   http.createServer(onRequest).listen(8888);
-  console.log("HTTP server has started!");
+  console.log("Server has started.");
 }
 
 exports.start = start;
