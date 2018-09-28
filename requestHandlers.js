d8ce8d4f6d5a8d11d35a4eb752683a0ed1f2a3f0
constantE <lianfengzhidie@sina.com>
2018-09-28 08:34:43 +0800

    修改requestHandlers，将数据包含在对upload请求的响应中

--- requestHandlers.js
+++ requestHandlers.js
@@ -19,7 +19,7 @@
 //   response.end();
 // }
 
-function start(response) {
+function start(response, postData) {
   console.log("Request handler 'start' was called.");
 
   var body = '<html>'+
@@ -40,10 +40,10 @@ function start(response) {
     response.end();
 }
 
-function upload(response) {
+function upload(response, postData) {
   console.log("Request handler 'upload' was called.");
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello Upload");
+  response.write("You've sent: " + postData);
   response.end();
 }
 
